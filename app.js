dfe65616f6943f6610afd43f4e4522f53da6fa9a
Naveed Ahmed <jaannaveed827@gmail.com>
2025-08-30 15:27:54 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var _ = require("lodash");
+
+const homeStartingContent =
+  "Lorem ipsum is a dummy text without any sense. It is a sequence of Latin words that, as they are positioned, do not form sentences with a complete sense, but give life to a test text useful to fill spaces that will subsequently be occupied from ad hoc texts composed by communication professionals.";
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    StartingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+//footer
+app.get("/footer", function (req, res) {
+  res.render("footer");
+});
+
+//Header
+app.get("/header", function (req, res) {
+  res.render("header");
+});
+//About Page
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+//Contact Page
+app.get("/contact", function (req, res) {
+  res.render("contact");
+});
+
+//compose  page
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  const newPost = {
+    title: req.body.postTitle,
+    content: req.body.content,
+  };
+  posts.push(newPost);
+  // Here you would typically save the newPost to your database
+  res.redirect("/");
+});
+
+app.get("/posts/:postTitle", function (req, res) {
+  const requestedPostTitle = _.lowerCase(req.params.postTitle);
+
+  posts.forEach(function (post) {
+    const postTitle = _.lowerCase(post.title);
+    if (postTitle === requestedPostTitle) {
+      res.render("post", {
+        post: post,
+      });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on http://localhost:3000");
+});
