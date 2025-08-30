e31dc5d821a408b2c6e2b0e5059e8f264209dd55
Naveed Ahmed <jaannaveed827@gmail.com>
2025-08-30 23:58:37 +0500

    Fix: use process.env.PORT for Heroku

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ app.get("/posts/:postTitle", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`);
 });
